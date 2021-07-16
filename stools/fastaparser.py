2a66b8a1c35cb93300caff2310f792ac9d83168e
yerv <yazatian@gmail.com>
2021-07-16 12:22:28 -0500

    fixed input to take non assembler outputted fasta

--- stools/fastaparser.py
+++ stools/fastaparser.py
@@ -3,26 +3,30 @@ import os.path
 import pandas as pd
 import plotly.graph_objects as go
 
-def loader(in_file):
+def loader(in_file, from_assembler):
     if os.path.isfile(in_file):
         df = pd.DataFrame(columns=["Scaffold", "Length", "SubScaffolds", "LengthofSubScaffolds"])
         record_dict = SeqIO.to_dict(SeqIO.parse(in_file, "fasta"))
-        df = data_wrangle(df, record_dict)
+        df = data_wrangle(df, record_dict, from_assembler)
         odirectory = os.path.dirname(in_file) + "/meta"
         if not os.path.exists(odirectory):
             os.makedirs(odirectory)
         save_csv(df, odirectory+"/final_fasta_meta.csv")
-        save_fig(df, odirectory+"/final_fasta_meta.png")
+        if from_assembler == "yes":
+            save_fig(df, odirectory+"/final_fasta_meta.png")
         print("DONE")
         return
     else:
         print("ERROR: Fafsa file does not exist!")
 
-def data_wrangle(df, record_dict):
-    for key in record_dict:
-        get_subscaff = record_dict[key].description.split(record_dict[key].name + " ")[1].split(",")
-        df = df.append({"Scaffold": record_dict[key].name  , "Length" : len(record_dict[key]), "SubScaffolds" : get_subscaff, "LengthofSubScaffolds" : len(get_subscaff) }, ignore_index=True)
-    
+def data_wrangle(df, record_dict, from_assembler):
+    if from_assembler == "yes":
+        for key in record_dict:
+            get_subscaff = record_dict[key].description.split(record_dict[key].name + " ")[1].split(",")
+            df = df.append({"Scaffold": record_dict[key].name  , "Length" : len(record_dict[key]), "SubScaffolds" : get_subscaff, "LengthofSubScaffolds" : len(get_subscaff) }, ignore_index=True)
+    else:
+        for key in record_dict:
+            df = df.append({"Scaffold": record_dict[key].name  , "Length" : len(record_dict[key]) }, ignore_index=True)
     if len(df) > 0:
         return df
     
